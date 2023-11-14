b35cfd8142ef343e16273d9e815f0f8d267dc600
jotix <jujodeve@gmail.com>
2023-11-14 20:55:32 -0300

    syncthing

--- configuration.nix
+++ configuration.nix
@@ -180,6 +180,24 @@
   # Enable fstrim
   services.fstrim.enable = true;
 
+  # Syncthing
+  services.syncthing = {
+    enable = true;
+    user = "jotix";
+    dataDir = "/home/jotix";
+    configDir = "/home/jotix/.config/syncthing";
+    devices."motorola_edge_30".id = "ZUMZLZC-AOEBXYB-UWHAYLK-UWCGUQ6-PFBLUGR-OIBHVRW-Y7JYEQT-XZA62QM";
+    folders = {
+      "Documents" = {
+        path = "/home/jotix/Documents";
+        devices = [ "motorola_edge_30" ];
+      };
+      "Pictures" = {
+        path = "/home/jotix/Pictures";
+        devices = [ "motorola_edge_30" ];
+      };
+    };
+  };
   # Flatpak
   #services.flatpak.enable = true;
 
