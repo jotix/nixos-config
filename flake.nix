48a5764ef822b4db1f36eb16b113150508848026
jotix <jujodeve@gmail.com>
2023-12-16 20:14:53 -0300

    update to 23.11

--- flake.nix
+++ flake.nix
@@ -3,8 +3,8 @@
   description = "jotix NixOS configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
-    home-manager.url = "github:nix-community/home-manager/release-23.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    home-manager.url = "github:nix-community/home-manager/release-23.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
