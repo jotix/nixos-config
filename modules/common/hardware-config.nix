7305410c2176f7d20f364df65c285437f123f706
jotix <69703151+jotix@users.noreply.github.com>
2023-12-30 11:10:03 -0300

    Update hardware-config.nix

--- modules/common/hardware-config.nix
+++ modules/common/hardware-config.nix
@@ -37,18 +37,6 @@
     fsType = "ntfs";
     #options = [ "user" "users" ];
   };
-  
-  fileSystems."/mnt/btrfs-vol-2" = {
-    device = "/dev/disk/by-label/btrfs-vol-2";
-    fsType = "btrfs";
-    options = [ "subvol=/" ];
-  };
-
-  fileSystems."/mnt/btrfs-vol-3" = {
-    device = "/dev/disk/by-label/btrfs-vol-3";
-    fsType = "btrfs";
-    options = [ "subvol=/" ];
-  };
 
   swapDevices = [ {
     device = "/var/lib/swapfile";
