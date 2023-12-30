c34b57998c37cc68dd0df90ddbb634a93ebc9eff
jotix <69703151+jotix@users.noreply.github.com>
2023-12-30 11:17:51 -0300

    Update config.nix

--- modules/common/config.nix
+++ modules/common/config.nix
@@ -172,27 +172,6 @@
 
   # Enable fstrim
   services.fstrim.enable = true;
-
-  # Syncthing
-  /*
-  services.syncthing = {
-    enable = true;
-    user = "jotix";
-    dataDir = "/home/jotix";
-    configDir = "/home/jotix/.config/syncthing";
-    devices."motorola_edge_30".id = "ZUMZLZC-AOEBXYB-UWHAYLK-UWCGUQ6-PFBLUGR-OIBHVRW-Y7JYEQT-XZA62QM";
-    folders = {
-      "Documents" = {
-        path = "/home/jotix/Documents";
-        devices = [ "motorola_edge_30" ];
-      };
-      "Pictures" = {
-        path = "/home/jotix/Pictures";
-        devices = [ "motorola_edge_30" ];
-      };
-    };
-  };
-  */
   
   # Flatpak
   #services.flatpak.enable = true;
@@ -212,17 +191,4 @@
   system.stateVersion = "23.11"; # Did you read the comment?
 
   hardware.keyboard.qmk.enable = true;
-
-  # udev rules for Arduino Pro Micro
-  # services.udev.extraRules = ''
-  #   # arduino pro micro rules
-  #   SUBSYSTEMS=="usb", ATTRS{idVendor}=="2341", ATTRS{idProduct}=="0037", MODE="0666"
-  #   SUBSYSTEMS=="usb", ATTRS{idVendor}=="2341", ATTRS{idProduct}=="0036", MODE="0666"
-  #   # jotanck rule
-  #   SUBSYSTEMS=="usb", ATTRS{idVendor}=="4a4f", ATTRS{idProduct}=="5458", MODE="0666"
-  #   # bm40hsrgb_v2 rule 4b50:1141 KP Republic BM40V2
-  #   SUBSYSTEMS=="usb", ATTRS{idVendor}=="4b50", ATTRS{idProduct}=="1141", MODE="0666"
-  #   # vial firmware rules
-  #   KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{serial}=="*vial:f64c2b3c*", MODE="0660", GROUP="users", TAG+="uaccess", TAG+="udev-acl"
-  #   '';
 }
