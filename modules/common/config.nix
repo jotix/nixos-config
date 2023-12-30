d745fd9fd87b1efda0a3573d4147bcb2343f4599
jotix <69703151+jotix@users.noreply.github.com>
2023-12-30 11:20:46 -0300

    Update config.nix

--- modules/common/config.nix
+++ modules/common/config.nix
@@ -97,51 +97,6 @@
   environment.shellAliases = {
     rebuild = "sudo nixos-rebuild switch --flake .#$HOSTNAME";
   };
-  
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-    usbutils
-    gnumake
-    cmake
-    gcc
-    wl-clipboard
-    xclip
-    wget
-    virt-manager
-    gnucash
-    gimp
-    python311Full
-    python311Packages.python-lsp-server
-    qmk
-    qmk-udev-rules
-    qmk_hid
-    zip
-    unzip
-    p7zip
-    librecad
-    libreoffice
-    mpv
-    gparted
-    htop
-    spotify
-    fuse
-    pass
-    inkscape
-    pciutils
-    vial
-    google-chrome
-    firefox
-    jetbrains-mono
-    ubuntu_font_family
-    neofetch
-    nerdfonts
-    dwt1-shell-color-scripts
-    gnome.gnome-tweaks
-    glade
-    #racket  
-    #guile
-  ]; # ++ (with lib; filter isDerivation (attrValues pkgs.plasma5Packages.kdeGear)); ## for install all kde apps
 
   # steam
   #programs.steam.enable = true;
