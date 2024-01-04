f16b962706e9a54e523118a26e34ea7cfe9dfa68
jotix <69703151+jotix@users.noreply.github.com>
2024-01-04 18:04:27 -0300

    extra filesystems

--- modules/hardware/hardware-config.nix
+++ modules/hardware/hardware-config.nix
@@ -43,6 +43,19 @@
     size = 4096;
   } ];
 
+  ## EXTRA FS
+  #fileSystems."/mnt/btrfs-vol-2" = {
+  #  device = "/dev/disk/by-label/btrfs-vol-2";
+  #  fsType = "btrfs";
+  #  options = [ "subvol=/" ];
+  #};
+
+  #fileSystems."/mnt/btrfs-vol-3" = {
+  #  device = "/dev/disk/by-label/btrfs-vol-3";
+  #  fsType = "btrfs";
+  #  options = [ "subvol=/" ];
+  #};
+
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
