47630e45da0f34beaf7c0ff285e2368ff919bd37
jotix <jujodeve@gmail.com>
2024-01-26 21:23:40 -0300

    typo

--- install.sh
+++ install.sh
@@ -44,7 +44,7 @@ sudo nixos-install --flake .#$@
 echo "Changing nixos-config origin repo"
 git remote remove origin
 git remote add origin git@github.com:jotix/nixos-config.git
-sudo cp -rv ../nixos-config/ /mnt/home/jotix/
+sudo cp -rv ../nixos-config /mnt/home/jotix/
 sudo nixos-enter --command 'chown -R jotix /home/jotix/nixos-config'
 
 ### set jotix's password
