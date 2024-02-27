2a568074d0ea13920182f5979c072b7a16578e49
jotix <jujodeve@gmail.com>
2024-02-27 07:04:16 -0300

    no sync mountpoints

--- modules/hardware/hardware-config.nix
+++ modules/hardware/hardware-config.nix
@@ -43,25 +43,6 @@
     size = 4096;
   } ];
 
-  ### Sync folders
-  fileSystems."/home/jotix/Documents" = {
-    device = "/dev/disk/by-label/jtx-system";
-    fsType = "btrfs";
-    options = [ "subvol=/sync/Documents" ];
-  };
-
-  fileSystems."/home/jotix/Pictures" = {
-    device = "/dev/disk/by-label/jtx-system";
-    fsType = "btrfs";
-    options = [ "subvol=/sync/Pictures" ];
-  };
-
-  fileSystems."/home/jotix/Camera" = {
-    device = "/dev/disk/by-label/jtx-system";
-    fsType = "btrfs";
-    options = [ "subvol=/sync/Camera" ];
-  };
-
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
