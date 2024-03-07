83cbda37b0911ecc5a62c7a687890812b3ccfbdb
jotix <jujodeve@gmail.com>
2024-03-07 11:27:08 -0300

    locale

--- configuration.nix
+++ configuration.nix
@@ -63,16 +63,15 @@
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF8";
 
+  i18n.supportedLocales = [
+    "en_US.UTF-8/UTF-8"
+    "es_AR.UTF-8/UTF-8"
+  ];
+
   i18n.extraLocaleSettings = {
-    LC_ADDRESS = "es_AR.UTF8";
-    LC_IDENTIFICATION = "es_AR.UTF8";
-    LC_MEASUREMENT = "es_AR.UTF8";
-    LC_MONETARY = "es_AR.UTF8";
-    LC_NAME = "es_AR.UTF8";
-    LC_NUMERIC = "es_AR.UTF8";
-    LC_PAPER = "es_AR.UTF8";
-    LC_TELEPHONE = "es_AR.UTF8";
-    LC_TIME = "es_AR.UTF8";
+    LANG     = "en_US.UTF8";
+    LANGUAGE = "es_AR.UTF8";
+    LC_ALL   = "es_AR.UTF8";
   };
 
   # Enable the X11 windowing system.
