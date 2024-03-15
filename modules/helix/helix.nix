e8a230b493eb5a5dbcab4ade1c4f99a0cde4d492
jotix <jujodeve@gmail.com>
2024-03-14 21:07:56 -0300

    helix

--- modules/helix/helix.nix
+++ modules/helix/helix.nix
@@ -3,19 +3,21 @@
 { config, pkgs, ... }:
 
 {
-  programs.helix = {
-    enable = true;
-    settings = {
-      theme = "tokyonight_storm";
-      editor.line-number = "relative";
-      keys.normal.esc = ["collapse_selection" "keep_primary_selection"];
+  home-manager.users.jotix = { pkgs, ... }:  {
+    programs.helix = {
+      enable = true;
+      settings = {
+        theme = "tokyonight_storm";
+        editor.line-number = "relative";
+        keys.normal.esc = ["collapse_selection" "keep_primary_selection"];
+      };
+      defaultEditor = true;
     };
-    defaultEditor = true;
-  };
 
-  programs.bash.shellAliases = {
-    vim = "hx";
-    vi = "hx";
-    nvim = "hx";
+    programs.bash.shellAliases = {
+      vim = "hx";
+      vi = "hx";
+      nvim = "hx";
+    };
   };
 }
