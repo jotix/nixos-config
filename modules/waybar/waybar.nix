95efef4395fc47efa9ca71857de1f34fcf833105
jotix <jujodeve@gmail.com>
2024-03-27 18:40:18 -0300

    waybar

---
+++ modules/waybar/waybar.nix
@@ -0,0 +1,38 @@
+
+{ config, pkgs, ... }:
+
+{
+  home-manager.users.jotix = { pkgs, ... }: {
+    programs.waybar = {
+      enable = true;
+      settings = {
+
+        mainBar = {
+          layer = "top";
+          position = "top";
+          height = 30;
+          output = [
+            "DP-1"
+          ];
+          modules-left = [ "sway/workspaces" "sway/mode" "wlr/taskbar" ];
+          modules-center = [ "sway/window" "custom/hello-from-waybar" ];
+          modules-right = [ "mpd" "custom/mymodule#with-css-id" "temperature" ];
+
+          "sway/workspaces" = {
+            disable-scroll = true;
+            all-outputs = true;
+          };
+          "custom/hello-from-waybar" = {
+            format = "hello {}";
+            max-length = 40;
+            interval = "once";
+            exec = pkgs.writeShellScript "hello-from-waybar" ''
+        echo "from within waybar"
+      '';
+          };
+        };
+      };
+    };
+      
+  };
+}
